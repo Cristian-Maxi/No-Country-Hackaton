90390da2796dffbe3bd39db5c22baaf5c2caf228
Cristian-Maxi <cristiangomez0686@outlook.com>
2024-10-06 05:26:31 -0300

    fix

--- backend/src/main/java/HackatonWaki/HackatonWakiApplication.java
+++ backend/src/main/java/HackatonWaki/HackatonWakiApplication.java
@@ -2,10 +2,8 @@ package HackatonWaki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "HackatonWaki.repositorys")
 public class HackatonWakiApplication {
 
 	public static void main(String[] args) {

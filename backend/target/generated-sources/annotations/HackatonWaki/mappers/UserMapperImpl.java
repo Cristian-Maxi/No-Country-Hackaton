4795f7a32ddb540a439810b9686f36d7f71e848e
Cristian-Maxi <cristiangomez0686@outlook.com>
2024-10-06 01:38:55 -0300

    test flyway

--- backend/target/generated-sources/annotations/HackatonWaki/mappers/UserMapperImpl.java
+++ backend/target/generated-sources/annotations/HackatonWaki/mappers/UserMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-10-06T00:57:34-0300",
+    date = "2024-10-06T01:37:48-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.2 (Oracle Corporation)"
 )
 @Component
